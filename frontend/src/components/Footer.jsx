f018dbb2b0c6b227f2646b38bc725687d42a4cc3
jjackson112 <jjackson11211@gmail.com>
2026-08-21 18:37:33 -0400

    footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="footer-container">
+            <h4>© Pau Hana Kitchen | 2026</h4>
+        </footer>
+    )
+}
+
+export default Footer;
