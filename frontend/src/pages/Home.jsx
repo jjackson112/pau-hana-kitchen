692a44e869d5c6902088ddf92d57f187b42bb8e7
jjackson112 <jjackson11211@gmail.com>
2026-08-21 19:27:57 -0400

    Home page structure

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+function Home() {
+    return (
+        <main>
+            <section className="hero-container">
+                <h1>Pau Hana Kitchen</h1>
+                <h2>Local favorites. Made fresh.</h2>
+                <p>Plate lunches, poke, loco moco, and more.</p>
+                <button className="cta-btn">
+                    Order now
+                </button>
+            </section>
+
+            <section className="popular-items"> 
+                <h2>Popular Dishes</h2>
+            </section>
+
+            <section className="menu-categories">
+                <h2>Menu Categories</h2>
+            </section>
+        </main>
+    )
+}
+
+export default Home;
