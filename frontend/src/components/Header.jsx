fe3aaecf763fe7c5a3930a08b8f2d96d1b585dc2
jjackson112 <jjackson11211@gmail.com>
2026-08-21 18:27:52 -0400

    toggle hamburger icon - add state

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X, Utensils } from "lucide-react";
 
 function Header() {
+    const [open, setOpen] = useState(false)
+
     return (
         <header className="header-container">
-            <button className="hamburger">
-                <Menu size={20} />
+            <button 
+                className="hamburger"
+                onClick={() => setOpen(!open)}
+            >
+                {open ? <X size={20} /> : <Menu size={20} />}
             </button>
 
             <div className="brand-icon">
@@ -15,7 +21,8 @@ function Header() {
 
             <div className="nav-menu">
                 <nav>
-                    <Link to="/dashboard">Home</Link>
+                    <Link to="/home">Home</Link>
+                    <Link to="/menu">Menu</Link>
                     <Link to="/orders">Orders</Link>
                     <Link to="/cart">Cart</Link>
                 </nav>
