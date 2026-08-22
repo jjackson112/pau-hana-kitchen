14ad51f80e16d3875956426fcae61b376871b601
jjackson112 <jjackson11211@gmail.com>
2026-08-21 20:49:39 -0400

    npm install react

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer-container">
-            <h4>© Pau Hana Kitchen | 2026</h4>
-        </footer>
-    )
-}
-
-export default Footer;
