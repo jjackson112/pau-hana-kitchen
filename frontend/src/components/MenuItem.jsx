7c2a81272659e5b3fc382605926913224f4a18e0
jjackson112 <jjackson11211@gmail.com>
2026-08-21 21:58:56 -0400

    MenuItem created

---
+++ frontend/src/components/MenuItem.jsx
@@ -0,0 +1,14 @@
+import menuItems from "../data/menu.js";
+
+function MenuItem({ item }) {
+    return (
+        <div>
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+            <p>{item.category}</p>
+        </div>
+    )
+}
+
+export default MenuItem;
