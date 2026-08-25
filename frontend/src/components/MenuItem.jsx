4c8f6086bd2c90a469b78a5bacc1fcfe04454640
jjackson112 <jjackson11211@gmail.com>
2026-08-24 22:33:12 -0400

    handleAddToCart for MenuItem

--- frontend/src/components/MenuItem.jsx
+++ frontend/src/components/MenuItem.jsx
@@ -1,8 +1,13 @@
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../store/cartSlice';
+import { Plus } from 'lucide-react';
 
 function MenuItem({ item }) {
     const dispatch = useDispatch()
+
+    const handleAddToCart = () => {
+        dispatch(addToCart(item)) // send the item as the action payload
+    }
 
     return (
         <div className="menu-item-card">
@@ -10,6 +15,7 @@ function MenuItem({ item }) {
             <p>{item.description}</p>
             <p>{item.price}</p>
             <p className="category-badge">{item.category}</p>
+            <button onClick={handleAddToCart}><Plus /></button>
         </div>
     )
 }
