2ead91679cb98ac84b5871e1ad80bb4eee7b8ced
jjackson112 <jjackson11211@gmail.com>
2026-08-24 22:54:01 -0400

    render logic for Cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -4,6 +4,13 @@ import { useSelector } from 'react-redux';
 function Cart() {
     const cartItems = useSelector((state) => state.cart.itemList)
     const totalQuantity = useSelector((state) => state.cart.totalQuantity)
+
+    return (
+        <main className="cart-page">
+            <h1 className="cart-title">Your Cart</h1>
+            <p>Total Items: {totalQuantity}</p>
+        </main>
+    )
 }
 
 export default Cart;
