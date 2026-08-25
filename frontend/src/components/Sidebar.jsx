069eea801ff4e0e3221d77348a84fe6f3be8d57a
jjackson112 <jjackson11211@gmail.com>
2026-08-25 12:07:16 -0400

    sidebar created - open sidebar when user is on Home adding menuItems to the cart

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import { X } from "lucide-react";
+
+// open the sidebar while user is on the homepage adding menu items to the cart
+
+function Sidebar() {
+    return (
+        <aside>
+            
+        </aside>
+    )
+}
+
+export default Sidebar;
