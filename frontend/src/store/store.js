1f34fd40939a9808a1a7f02353603761d1edc91b
jjackson112 <jjackson11211@gmail.com>
2026-08-30 20:22:49 -0400

    set localStorage + use stringify to convert value to string

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -9,6 +9,11 @@ export const store = configureStore({
 
 store.subscribe(() => {
     const cartState = store.getState().cart
+
+    localStorage.setItem(
+        "cart",
+        JSON.stringify(cartState)
+    )
 })
 
 export default store;
