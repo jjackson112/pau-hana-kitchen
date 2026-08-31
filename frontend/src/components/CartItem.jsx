c14496d8364d8d05b0ef7950eb211644a0acdea1
jjackson112 <jjackson11211@gmail.com>
2026-08-31 12:07:10 -0400

    handle the quantities on CartItem

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -1,22 +1,42 @@
 import { useDispatch } from "react-redux";
+import { addToCart, removeFromCart } from "../store/cartSlice";
 
 function CartItem({ item }) {
     const dispatch = useDispatch()
 
+    const handleIncrease = () => {
+        dispatch(addToCart(item))
+    }
+
+    const handleDecrease = () => {
+        dispatch(removeFromCart(item.id))
+    }
 
     return (
         <article className="cart-item">
             <div className="cart-item-info">
                 <h4>{item.name}</h4>
-                <p>{item.description}</p>
+                <p>${item.totalPrice.toFixed(2)} each</p>
             </div>
 
             <div className="cart-item-quantity">
-                <span>Quantity: {item.quantity}</span>
+                <button 
+                    className="increase-quantity-btn"
+                    onClick={handleIncrease}
+                >
+                    +
+                </button>
+                <span>{item.quantity}</span>
+                <button 
+                    className="decrease-quantity-btn"
+                    onClick={handleDecrease}
+                >
+                    -
+                </button>
             </div>
 
             <div className="cart-item-total">
-                ${item.totalPrice.toFixed(2)}
+                <p>${item.totalPrice.toFixed(2)}</p>
             </div>
         </article>
     )
