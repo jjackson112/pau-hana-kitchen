99a12be3062e022746c0d3587e893640cad3c122
jjackson112 <jjackson11211@gmail.com>
2026-08-31 12:28:21 -0400

    added Cart styles + added fees section

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -1,42 +1,40 @@
 import { useDispatch } from "react-redux";
 import { addToCart, removeFromCart } from "../store/cartSlice";
+import { Plus, Minus } from "lucide-react";
 
 function CartItem({ item }) {
     const dispatch = useDispatch()
-
-    const handleIncrease = () => {
-        dispatch(addToCart(item))
-    }
 
     const handleDecrease = () => {
         dispatch(removeFromCart(item.id))
     }
 
+    const handleIncrease = () => {
+        dispatch(addToCart(item))
+    }
+
     return (
-        <article className="cart-item">
+        <article className="total-cart-item">
             <div className="cart-item-info">
                 <h4>{item.name}</h4>
-                <p>${item.totalPrice.toFixed(2)} each</p>
+                <p>{item.description}</p>
+                <p>${item.totalPrice.toFixed(2)}</p>
             </div>
 
             <div className="cart-item-quantity">
                 <button 
-                    className="increase-quantity-btn"
-                    onClick={handleIncrease}
-                >
-                    +
-                </button>
-                <span>{item.quantity}</span>
-                <button 
                     className="decrease-quantity-btn"
                     onClick={handleDecrease}
                 >
-                    -
+                    <Minus size={10} />
                 </button>
-            </div>
-
-            <div className="cart-item-total">
-                <p>${item.totalPrice.toFixed(2)}</p>
+                <span>{item.quantity}</span>
+                <button 
+                    className="increase-quantity-btn"
+                    onClick={handleIncrease}
+                >
+                    <Plus size={10} />
+                </button>
             </div>
         </article>
     )
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -13,18 +13,29 @@ function Cart() {
             <h1 className="cart-title">Your Cart</h1>
             <p><strong>Total Items: {totalQuantity}</strong></p>
 
-            {cartItems.length === 0 ? (
-                <p>Cart is empty</p>
-            ) :
-            <div className="cart-items">
-                {cartItems.map((item) => (
-                    <CartItem 
-                        key={item.id}
-                        item={item}
-                    />
-                ))}
-            </div>
-            }   
+            <div className="cart-items-list">
+                {cartItems.length === 0 ? (
+                    <p>Cart is empty</p>
+                ) :
+                <div>
+                    {cartItems.map((item) => (
+                        <CartItem 
+                            key={item.id}
+                            item={item}
+                        />
+                    ))}
+                </div>
+                }
+            </div>  
+
+            <div className="fees">
+                <h4 className="subtotal">Subtotal</h4>
+                <h4 className="delivery-fees">Delivery Fees</h4>
+                <h4 className="tax">Tax</h4>
+                <h4 className="tip">Tip</h4>
+                <p className="tip-options"></p>
+                <h4 className="total">Total</h4>
+            </div> 
         </main>
     )
 }
