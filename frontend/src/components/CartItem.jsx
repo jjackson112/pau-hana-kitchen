2bd33aae128ebc4ba63563c9bb90ab11b2271dd2
jjackson112 <jjackson11211@gmail.com>
2026-08-31 11:58:39 -0400

    import useDispatch to dispatch the Redux action

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -1,4 +1,9 @@
+import { useDispatch } from "react-redux";
+
 function CartItem({ item }) {
+    const dispatch = useDispatch()
+
+
     return (
         <article className="cart-item">
             <div className="cart-item-info">
