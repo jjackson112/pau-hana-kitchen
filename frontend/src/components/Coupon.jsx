ea90dc0a63d144d7e109fc3f6c83995f07ab73ca
jjackson112 <jjackson11211@gmail.com>
2026-09-03 14:12:27 -0400

    improve coupon container

--- frontend/src/components/Coupon.jsx
+++ frontend/src/components/Coupon.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 function Coupon() {
+    const [coupon, setCoupon] = useState("")
+
     return (
         <section className="coupon-container">
             <h3 className="coupon-title">Save here</h3>
+            
             <input 
-                className=""
-                type="number"
+                className="coupon-input-field"
+                type="text"
                 placeholder="Enter coupon code"
-                value={}
-                onChange={}
+                value={coupon}
+                onChange={(e) => (e.target.value.toUpperCase())}
             />
+            
+            <button
+                className="coupon-btn"
+                type="button"
+            >
+                Apply
+            </button>
         </section>
     )
 }
+
+export default Coupon;
