07f8d6d9b61646205049f2001b85a1637231ff2c
jjackson112 <jjackson11211@gmail.com>
2026-09-03 22:05:56 -0400

    map over the menu items to display a more detailed cart summary

--- frontend/src/components/CartSummary.jsx
+++ frontend/src/components/CartSummary.jsx
@@ -1,15 +1,32 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import MenuItem from "./MenuItem";
 import { Dot, Utensils } from "lucide-react";
 
 function CartSummary() {
+    const dispatch = useDispatch()
+    const cartItems = useSelector((state) => state.cart.itemList)
     const totalQuantity = useSelector((state) => state.cart.totalQuantity)
 
     return (
         <section className="cart-summary">
-            <Utensils />
-            <h3>Pau Hana Kitchen</h3>
-            <Dot aria-hidden="true" />
-            <p>{totalQuantity} {totalQuantity === 1 ? "item" : "items"}</p> 
+            <div className="cart-summary-header">
+                <Utensils aria-hidden="true"/>
+                <h3>Pau Hana Kitchen</h3>
+                <Dot aria-hidden="true" />
+                <p>{totalQuantity} {totalQuantity === 1 ? "item" : "items"}</p> 
+            </div>
+
+            <div className="cart-summary-items">
+                {cartItems.map((item) => (
+                    <div className="cart-summary-item" key={item.id}>
+                        <div>
+                            <p>{item.name}</p>
+                            <p>x {item.quantity}</p>
+                        </div>
+                        <p>${item.totalPrice.toFixed(2)}</p>
+                    </div>
+                ))}
+            </div>
         </section>
     )
 }
