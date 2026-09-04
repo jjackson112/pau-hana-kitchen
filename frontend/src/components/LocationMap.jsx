0b41d1bfaba7a3c8798ead7aea7033ab082af8ad
jjackson112 <jjackson11211@gmail.com>
2026-09-04 10:00:35 -0400

    use encodeURIcomponent for mapQuery - Location component created

---
+++ frontend/src/components/LocationMap.jsx
@@ -0,0 +1,12 @@
+import { MapPin } from "lucide-react";
+
+function LocationMap({ orderType }) {
+    const restaurantAddress = "Hana Town, HI"
+    const mapQuery = encodeURIComponent(restaurantAddress)
+
+    return (
+        <section className="location-map">
+            
+        </section>
+    )
+}
