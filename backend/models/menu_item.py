87be98eb9cbba9bd688e6daededc75eeb0bf616a
jjackson112 <jjackson11211@gmail.com>
2026-09-09 17:04:23 -0400

    choose one id in Menu Item model + give Boolean properties to appropriate columsn

--- backend/models/menu_item.py
+++ backend/models/menu_item.py
@@ -2,19 +2,16 @@ from extensions import db
 
 class MenuItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    menu_item_id = db.Column(db.Integer, nullable=True)
-
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     price = db.Column(db.Numeric(10, 2), nullable=False)
     category = db.Column(db.String(100), nullable=False)
-    popular = db.Column(db.String(25), nullable=False)
-    vegetarian = db.Column(db.String(25), nullable=False)
+    popular = db.Column(db.Boolean, nullable=False, default=False)
+    vegetarian = db.Column(db.Boolean, nullable=False, default=False)
 
     def to_dict(self):
         return {
             "id": self.id,
-            "menu_item_id": self.menu_item_id,
             "name": self.name,
             "description": self.description,
             "price": float(self.price),
