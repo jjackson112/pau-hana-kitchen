5a2b06290207583a9177b5ba9d5addc4c150af5e
jjackson112 <jjackson11211@gmail.com>
2026-09-09 16:53:56 -0400

    rm menu_items line in Order Items model + create Menu Items model for Order Items to reference and focus on what restaurant sells

--- backend/models/order_item.py
+++ backend/models/order_item.py
@@ -4,7 +4,6 @@ class Order_Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     order_id = db.Column(db.Integer, db.ForeignKey("order.id", nullable=False))
 
-    menu_item_id = db.Column(db.Integer, nullable=True)
     name = db.Column(db.String(150), nullable=False)
     price = db.Column(db.Numeric(10,2), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
@@ -15,7 +14,6 @@ class Order_Item(db.Model):
         return {
             "id": self.id,
             "order_id": self.order_id,
-            "menu_item_id": self.menu_item_id,
             "name": self.name,
             "price": float(self.price),
             "quantity": self.quantity
