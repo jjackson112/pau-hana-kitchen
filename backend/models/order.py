dc4c1524c7bf8c1a65a77b7be18d0b2aa739781a
jjackson112 <jjackson11211@gmail.com>
2026-09-09 14:43:26 -0400

    to dict() for Order + OrderItem models

--- backend/models/order.py
+++ backend/models/order.py
@@ -5,18 +5,37 @@ class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id", nullable=False))
 
-    order_type = db.Column()
-    order_items = db.Column()
-    subtotal = db.Column()
-    discount = db.Column()
-    tax = db.Column()
-    tip = db.Column()
-    total = db.Column()
+    order_type = db.Column(db.String(25), nullable=False)
+    order_items = db.Column(db.Integer, nullable=False)
+    subtotal = db.Column(db.Integer, nullable=False)
+    discount = db.Column(db.Integer, nullable=False)
+    tax = db.Column(db.Integer, nullable=False)
+    tip = db.Column(db.Integer, nullable=False)
+    total = db.Column(db.Integer, nullable=False)
 
-    customer_name = db.Column()
-    customer_email = db.Column()
+    customer_name = db.Column(db.String(150), nullable=False)
+    customer_email = db.Column(db.String(100), nullable=False)
     customer_phone_number = db.Column()
-    delivery_address = db.Column()
+    delivery_address = db.Column(db.Text, nullable=False)
 
     created_at = db.Column(db.Datetime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.Datetime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "order_type": self.order_type,
+            "order_items": self.order_items,
+            "subtotal": self.subtotal,
+            "discount": self.discount,
+            "tax": self.tax,
+            "tip": self.tip,
+            "total" : self.total,
+            "customer_name": self.customer_name,
+            "customer_email": self.customer_email,
+            "customer_phone_number": self.customer_phone_number,
+            "delivery_address": self.delivery_address,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
--- backend/models/order_item.py
+++ backend/models/order_item.py
@@ -4,6 +4,15 @@ class Order_Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     order_id = db.Column(db.Integer, db.ForeignKey("order.id", nullable=False))
 
-    name = db.Column()
-    price = db.Column()
-    quantity = db.Column()
+    name = db.Column(db.String(500), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "name": self.name,
+            "price": self.price,
+            "quantity": self.quantity
+        }
