5db5431b3665271f11b39e350c46dcfe8a7c73b8
jjackson112 <jjackson11211@gmail.com>
2026-09-09 15:23:28 -0400

    edits to models - db.Numeric(10,2)

--- backend/models/order.py
+++ backend/models/order.py
@@ -3,19 +3,18 @@ from datetime import datetime
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id", nullable=False))
+    customer_id = db.Column(db.Integer, db.ForeignKey("user.id", nullable=False))
 
     order_type = db.Column(db.String(25), nullable=False)
-    order_items = db.Column(db.Integer, nullable=False)
-    subtotal = db.Column(db.Integer, nullable=False)
-    discount = db.Column(db.Integer, nullable=False)
-    tax = db.Column(db.Integer, nullable=False)
-    tip = db.Column(db.Integer, nullable=False)
-    total = db.Column(db.Integer, nullable=False)
+    subtotal = db.Column(db.Numeric(10,2), nullable=False)
+    discount = db.Column(db.Numeric(10,2), nullable=False, default=0)
+    tax = db.Column(db.Numeric(10,2), nullable=False)
+    tip = db.Column(db.Numeric(10,2), nullable=False, default=0)
+    total = db.Column(db.Numeric(10,2), nullable=False)
 
     customer_name = db.Column(db.String(150), nullable=False)
     customer_email = db.Column(db.String(100), nullable=False)
-    customer_phone_number = db.Column()
+    customer_phone_number = db.Column(db.String(30), nullable=True)
     delivery_address = db.Column(db.Text, nullable=False)
 
     created_at = db.Column(db.Datetime, nullable=False, default=datetime.utcnow)
@@ -24,9 +23,8 @@ class Order(db.Model):
     def to_dict(self):
         return {
             "id": self.id,
-            "user_id": self.user_id,
+            "user_id": self.customer_id,
             "order_type": self.order_type,
-            "order_items": self.order_items,
             "subtotal": self.subtotal,
             "discount": self.discount,
             "tax": self.tax,
--- backend/models/order_item.py
+++ backend/models/order_item.py
@@ -4,14 +4,16 @@ class Order_Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     order_id = db.Column(db.Integer, db.ForeignKey("order.id", nullable=False))
 
-    name = db.Column(db.String(500), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
+    menu_item_id = db.Column(db.Integer, nullable=True)
+    name = db.Column(db.String(150), nullable=False)
+    price = db.Column(db.Numeric(10,2), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
 
     def to_dict(self):
         return {
             "id": self.id,
             "order_id": self.order_id,
+            "menu_item_id": self.menu_item_id,
             "name": self.name,
             "price": self.price,
             "quantity": self.quantity
