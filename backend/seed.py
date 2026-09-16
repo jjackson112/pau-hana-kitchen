061a4a384229856a125e69e0850dcf004646571e
jjackson112 <jjackson11211@gmail.com>
2026-09-16 15:41:31 -0400

    add Flask to seed.py - create the app + db operations

--- backend/seed.py
+++ backend/seed.py
@@ -1,21 +1,26 @@
 import json
+
+from app import create_app
 from extensions import db
 from models.menu_item import MenuItem
 
-# read menu data from frontend + convert to dictionary
+app = create_app()
+
+# read menu data from frontend into a Python list
 with open("data/menu.json", "r") as file:
     menu_items = json.load(file)
 
-# create a MenuItem loop 
-for item in menu_items:
-    menu_item = MenuItem(
-        name = item["name"],
-        description = item["description"],
-        price = item["price"],
-        category = item["category"],
-        popular = item["popular"],
-        vegetarian = item["vegetarian"]
-    )
-    db.session.add(menu_item)
+with app.app_context():
+    # create a MenuItem loop 
+    for item in menu_items:
+        menu_item = MenuItem(
+            name = item["name"],
+            description = item["description"],
+            price = item["price"],
+            category = item["category"],
+            popular = item["popular"],
+            vegetarian = item["vegetarian"]
+        )
+        db.session.add(menu_item)
 
 db.session.commit()
