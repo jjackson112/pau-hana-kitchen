9829fd0afb7b47cc2ec90c0fe459b84e40b78c99
jjackson112 <jjackson11211@gmail.com>
2026-09-16 15:26:35 -0400

    create a MenuItem loop + add model object

--- backend/seed.py
+++ backend/seed.py
@@ -2,5 +2,20 @@ import json
 from extensions import db
 from models.menu_item import MenuItem
 
+# read menu data from frontend + convert to dictionary
 with open("data/menu.json", "r") as file:
     menu_items = json.load(file)
+
+# create a MenuItem loop 
+for item in menu_items:
+    menu_item = MenuItem(
+        name = item["name"],
+        description = item["description"],
+        price = item["item"],
+        category = item["category"],
+        popular = item["popular"],
+        vegetarian = item["vegetarian"]
+    )
+
+db.session.add(menu_item)
+db.session.commit()
