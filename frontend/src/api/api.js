273bf64541ddb5c92f3bd7bc979a2d1f6d951202
jjackson112 <jjackson11211@gmail.com>
2026-09-18 17:20:41 -0400

    request function + all endpoint requests

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -19,3 +19,27 @@ const request = async (endpoint, options={}) => {
 
         return data
     }
+
+export const api = {
+    get: (endpoint) => 
+        request(endpoint, {
+            method: "GET",
+        }),
+
+    post: (endpoint, body) =>
+        request(endpoint, {
+            method: "POST",
+            body: JSON.stringify(body),
+        }),
+
+    patch: (endpoint, body) =>
+        request(endpoint, {
+            method: "PATCH",
+            body: JSON.stringify(body),
+        }),
+    
+    delete: (endpoint) =>
+        request(endpoint, {
+            method: "DELETE",
+        }),
+}
