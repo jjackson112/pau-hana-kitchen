bfc2f6c9cff0d7ebce76b039d58bbae657b30fd1
jjackson112 <jjackson11211@gmail.com>
2026-09-22 22:29:56 -0400

    fix OrderDetail route in App.jsx

--- frontend/src/components/OrderDetail.jsx
+++ frontend/src/components/OrderDetail.jsx
@@ -19,7 +19,7 @@ function OrderDetail() {
                 const data = await api.get(`/orders/${id}`)
 
                 console.log("Order details fetched", data)
-                setOrderDetails(data)
+                setOrderDetails(data || [])
 
             } catch (err) {
                 console.log("Failed to fetch order details", err)
